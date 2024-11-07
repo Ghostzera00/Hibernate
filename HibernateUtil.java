49d4b9a2869dba6433caeb21125d6c9943a8b894
Ghostzera00 <146647700+Ghostzera00@users.noreply.github.com>
2024-11-07 14:06:01 -0300

    HibernateUtil.java

---
+++ HibernateUtil.java
@@ -0,0 +1,28 @@
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil
+{
+    private static SessionFactory sessionFactory = null;
+    public static String mensagem = "";
+    
+    static 
+    {
+        try
+        {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        catch (HibernateException ex)
+        {
+            mensagem = ex.getMessage();
+        }
+    }
+
+    public static SessionFactory getSessionFactory()
+    {
+        return sessionFactory;
+    }
+}
